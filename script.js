11abec6b739ec642304731db6c25493cacfaa870
firio3o <95288649+firio3o@users.noreply.github.com>
2021-12-09 04:15:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+// Code for spin
+const wheel = document.querySelector('.wheel');
+const startButton = document.querySelector('.button');
+const arrow = document.querySelector('.pin');
+
+let deg = 0;
+
+
+startButton.onclick = function () {
+	deg = Math.floor(5000 + Math.random() * 5000);
+	wheel.style.transition = 'all 10s ease-out';
+	wheel.style.transform = `rotate(${deg}deg)`;
+	wheel.classList.add('blur');
+            }
+
+wheel.addEventListener('transitionend', () => {
+	wheel.classList.remove('blur')
+	wheel.style.transition = 'none';
+	const actualDeg = deg % 360;
+	wheel.style.transform = `rotate(${actualDeg}deg)`;
+	arrow.classList.add('bounce')
+});
+
+
+//let audio = new Audio('tick.mp3')
+
+//function playSound()
+//{
+//        audio.currentTime = 0;
+//       audio.play();
+//        audio.loop = true;
+//}
+
+//function stopSound()
+//{
+//	audio.pause();
+//}
